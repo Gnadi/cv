303393db94e70813a0a31a0d9159bb6f6e348a3a
Gnadi <johannes.gnadlinger1997@gmail.com>
2024-04-19 17:16:16 +0200

    removed projects from print (to stay on a single page)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -154,7 +154,7 @@ export default function Page() {
           </div>
         </Section>
 
-        <Section className="print-force-new-page scroll-mb-16">
+        <Section className="print-force-new-page scroll-mb-16 print:hidden">
           <h2 className="text-xl font-bold">Projects</h2>
           <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
             {RESUME_DATA.projects.map((project) => {
